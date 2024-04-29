e02997a18e5f1dd5316d59c159f524a3a3ae998a
Nikita <deryabin2001nikita@gmail.com>
2024-04-30 02:52:07 +0300

    environments

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    coreUrl: '',
+    notifierUrl: '/notifier',
+    consoleUrl: 'http://localhost:44567',
+    googleClientId: '914510104037-k5ft2q9qjia33jgmh9mo3nm99ea04kbn.apps.googleusercontent.com',
+};
