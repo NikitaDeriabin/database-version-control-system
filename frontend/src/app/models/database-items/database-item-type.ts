3b78a79c32c1ffc0c0dbb8880b20a48ff1c867c3
Nikita <deryabin2001nikita@gmail.com>
2024-04-30 02:59:30 +0300

    models for database items

---
+++ frontend/src/app/models/database-items/database-item-type.ts
@@ -0,0 +1,8 @@
+export enum DatabaseItemType {
+    Table,
+    Type,
+    Constraint,
+    StoredProcedure,
+    Function,
+    View
+}
---
+++ frontend/src/app/models/database-items/database-item.ts
@@ -0,0 +1,7 @@
+import { DatabaseItemType } from './database-item-type';
+
+export interface DatabaseItem {
+    name: string;
+    type: DatabaseItemType;
+    schema: string;
+}
